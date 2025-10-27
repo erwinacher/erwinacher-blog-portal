244e9b72d00b84a5de3a6c35adfd310a1e347352
Ervin Bosenbacher <ervin.bosenbacher@rvnllm.com>
2025-10-27 07:54:13 +0000

    blog utils functions done, UNTESTED

---
+++ src/app/blog/utils.ts
@@ -0,0 +1,74 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+// get all the mdx files from the content directory
+function getMDXFiles(p: string) {
+    return fs.readdirSync(p).filter((file) => path.extname(file) === ".mdx");
+}
+
+// read data from those files/read content of those files
+function readMDXFile(filePath: fs.PathOrFileDescriptor) {
+    let rawContent = fs.readFileSync(filePath, "utf-8");
+    return matter(rawContent);
+}
+
+// present and render the mdx data and metadata
+function getMDXData(p: string) {
+    let mdxFiles = getMDXFiles(p);
+
+    return mdxFiles.map((file) => {
+        let { data: metadata, content } = radMDXFile(path.join(p, file));
+        let slug = path.basename(file, path.extname(file));
+
+        return {
+            metadata,
+            slug, 
+            content,
+        };
+    });
+}
+
+export function getBlogPosts() {
+    return getMDXData(path.join(process.cwd(), "src", "app", "blog", "contents"));
+}
+
+export function formatDate(date: string, includeRelative = false) {
+    let currentDate = new Date();
+    if (!date.includes('T')) {
+        date = `${date}T00:00:00`;
+    }
+    
+    let targetDate = new Date(date);
+
+    let yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
+    let monthsAgo = currentDate.getMonth() - targetDate.getMonth();
+    let daysAgo = currentDate.getDate() - targetDate.getDate();
+
+    let formattedDate = "";
+    if (yearsAgo > 0) {
+        formattedDate = `${yearsAgo}y ago`;
+    }
+    else if (monthsAgo > 0) {
+        formattedDate = `${monthsAgo}mo ago`;
+    }
+    else if (daysAgo > 0) {
+        formattedDate = `${daysAgo}d ago`;
+    } else {
+        formattedDate = "Today";
+    }
+
+    let fullDate = targetDate.toLocaleString("en-us"), {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+    });
+
+    if (!includeRelative) {
+        return fullDate;
+    }
+
+    return `${fullDate} (${formattedDate})`;
+}
+
+
